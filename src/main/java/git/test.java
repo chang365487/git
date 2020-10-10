8ceedfc2c89ae7b3a7714c8d5b7e0fbeb95c753d
xiaochang <xiaochang.com>
2020-10-10 08:48:27 +0800

    do git commit

---
+++ src/main/java/git/test.java
@@ -0,0 +1,11 @@
+package git;
+
+/**
+ * @author shkstart
+ * @create 2020-10-10 8:34
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello work");
+    }
+}
